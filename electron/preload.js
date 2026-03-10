463689a733b7a5638c254843fde742cd58e2b19a
lreddell <loganjreddell@gmail.com>
2026-03-10 03:18:36 -0400

    feat: add panorama path retrieval and enhance tree generation logic with new sapling types

--- electron/preload.js
+++ electron/preload.js
@@ -5,6 +5,7 @@ contextBridge.exposeInMainWorld('atlasDesktop', {
   readPanorama: (filePath) => ipcRenderer.invoke('panorama:read', { filePath }),
   pickPanorama: () => ipcRenderer.invoke('panorama:pick'),
   deletePanorama: (filePath) => ipcRenderer.invoke('panorama:delete', { filePath }),
+  getDefaultPanoramaPath: () => ipcRenderer.invoke('panorama:getDefaultPath'),
   listWorldPresets: () => ipcRenderer.invoke('worldPreset:list'),
   readWorldPreset: (id) => ipcRenderer.invoke('worldPreset:read', { id }),
   saveWorldPreset: (name, config) => ipcRenderer.invoke('worldPreset:save', { name, config }),
